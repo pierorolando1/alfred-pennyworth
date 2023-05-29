090beb87b788a21a965052499a95e5122412188b
Piero Rolando <rolandopiero63@gmail.com>
2023-05-29 14:46:00 -0500

    Add real-time updating to the chart and display current focus level with a marker

--- lib/features/focuslevel/main.dart
+++ lib/features/focuslevel/main.dart
@@ -1,12 +1,13 @@
 import 'dart:math';
 
-double calcFocus(DateTime wakeUpTime, int after) {
+double calcFocus(int after) {
   return 50 + (50 * sin((2 * pi) / 90 * (after - 22.5)));
 }
 
 class ChartData {
-  ChartData(this.xValue, this.lowValue, this.highValue, this.dataLabel);
+  ChartData(this.xValue, this.yValue, this.lowValue, this.highValue, this.dataLabel);
   final num xValue;
+  final num yValue;
   final double lowValue;
   final double highValue;
   final String dataLabel;
@@ -15,13 +16,14 @@ class ChartData {
 /// [ [min, max] ]
 List<ChartData> generateFocusTimeline(DateTime wakeUpTime) {
   final now = DateTime.now();
-  final end = DateTime(now.year, now.month, now.day, 20, 0, 0, 0, 0);
+  
+  final end = DateTime(now.year, now.month, now.day, 21, 0, 0, 0, 0);
   final diff = end.difference(wakeUpTime);
 
   final List<ChartData> timeline = [];
 
   for (var i = 0; i < diff.inMinutes; i += 5) {
-    final focusLevel = calcFocus(wakeUpTime, i);
+    final focusLevel = calcFocus(i);
 
     final num d = 6 * (1 + (i / diff.inMinutes));
 
@@ -30,6 +32,7 @@ List<ChartData> generateFocusTimeline(DateTime wakeUpTime) {
     timeline.add(
       ChartData(
         i,
+        focusLevel,
         max(0, focusLevel - d),
         min(100, focusLevel + d),
         "${ addedTime.hour }:${ addedTime.minute }",
@@ -38,4 +41,26 @@ List<ChartData> generateFocusTimeline(DateTime wakeUpTime) {
   }
 
   return timeline;
+}
+
+calcCurrFocus(DateTime wakeUpTime) {
+  final now = DateTime.now();
+  final diff = now.difference(wakeUpTime);
+
+  int minutes = now.minute;
+
+  if(minutes % 5 == 0) return [ChartData(diff.inMinutes, calcFocus(diff.inMinutes ), 0, 100, "${ now.hour }:${ now.minute }")];
+  
+  if(minutes > 55) {
+    minutes = minutes - (minutes%5);
+    return [ChartData(diff.inMinutes, calcFocus(diff.inMinutes), 0, 100, "${ now.hour }:$minutes")];
+  }
+
+  if(minutes%5 < ( 5 - (minutes%5))) {
+      minutes = minutes - (minutes%5);
+  } else {
+      minutes = minutes + ( 5 - (minutes%5));
+  }
+
+  return [ChartData(diff.inMinutes, calcFocus(diff.inMinutes), 0, 100, "${ now.hour }:$minutes")];
 }
---
+++ lib/features/focuslevel/widgets/focus_levels.dart
@@ -0,0 +1,99 @@
+import 'dart:async';
+
+import 'package:alfred/features/focuslevel/main.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class FocusLevels extends StatefulWidget {
+  final DateTime wakeUpTime;
+  
+  const FocusLevels({
+    super.key,
+    required this.wakeUpTime,
+  });
+
+  @override
+  State<FocusLevels> createState() => _FocusLevelsState();
+}
+
+class _FocusLevelsState extends State<FocusLevels> {
+  // calc with wakeUpTime Property
+  List currTime = [];
+  late Timer timer;
+
+  @override
+  void initState() {
+    currTime = calcCurrFocus(widget.wakeUpTime);
+    
+    timer = Timer.periodic(const Duration(minutes: 1), (timer) {
+      setState(() {
+        currTime = calcCurrFocus(widget.wakeUpTime);
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        margin: const EdgeInsets.all(17),
+        child: Container(
+          padding: const EdgeInsets.all(13),
+          height: 290,
+          width: 900,
+          child: SfCartesianChart(
+            zoomPanBehavior: ZoomPanBehavior(
+              enableDoubleTapZooming: true,
+              enablePinching: true,
+              zoomMode: ZoomMode.x,
+              enablePanning: true,
+              enableMouseWheelZooming: true,
+            ),
+            plotAreaBorderWidth: 0,
+            primaryXAxis: CategoryAxis(
+              interval: 4,
+              majorGridLines: const MajorGridLines(width: 0),
+              labelPlacement: LabelPlacement.onTicks,
+            ),
+            primaryYAxis: NumericAxis(
+              minimum: 0,
+              maximum: 100,
+              axisLine: const AxisLine(width: 0),
+              edgeLabelPlacement: EdgeLabelPlacement.shift,
+              labelFormat: '{value}%',
+              majorTickLines: const MajorTickLines(size: 0),
+            ),
+            series: [
+              SplineSeries<ChartData, String>(
+                color: const Color.fromRGBO(75, 135, 185, 0.5),
+                dataSource: generateFocusTimeline(
+                  widget.wakeUpTime,
+                ),
+                xValueMapper: (ChartData data, _) =>
+                    data.dataLabel,
+                yValueMapper: (datum, index) => datum.yValue,
+              ),
+              ScatterSeries<ChartData, String>(
+                  dataSource: calcCurrFocus(widget.wakeUpTime),
+                  opacity: 0.7,
+                  xValueMapper: (ChartData data, _) =>
+                      data.dataLabel,
+                  yValueMapper: (ChartData data, _) =>
+                      data.yValue,
+                  markerSettings:
+                      const MarkerSettings(height: 18, width: 18),
+                  name: 'Now'),
+            ],
+            tooltipBehavior: TooltipBehavior(enable: true),
+          ),
+        )
+        );
+  }
+}
