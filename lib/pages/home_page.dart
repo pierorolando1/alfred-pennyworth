c0765d2f6162a2485d774a49c90cefa23cebc5bc
Piero Rolando <rolandopiero63@gmail.com>
2023-05-29 14:45:54 -0500

    Add home page and update routes with new pages and widgets

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:alfred/features/focuslevel/widgets/focus_levels.dart';
+
+final today = DateTime.now();
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 80),
+            const Text(
+              "Welcome young Piero",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            FocusLevels(
+              //today at 7:20 am
+              wakeUpTime: DateTime(today.year, today.month, today.day, 6, 10),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,10 +1,10 @@
 import 'package:alfred/features/auth/pages/loading_page.dart';
 import 'package:alfred/features/auth/pages/login_page.dart';
-import 'package:alfred/features/focuslevel/main.dart';
+import 'package:alfred/features/tasks/pages/tasks_page.dart';
+import 'package:alfred/pages/home_page.dart';
 import 'package:alfred/shared/widgets/redirect.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 
 final mainRouter = GoRouter(
   routes: [
@@ -31,51 +31,10 @@ final mainRouter = GoRouter(
           ShellRoute(
             routes: [
               GoRoute(
-                path: "home",
-                builder: (context, state) => Scaffold(
-                  body: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SfCartesianChart(
-                        plotAreaBorderWidth: 0,
-                        primaryXAxis: CategoryAxis(
-                            majorGridLines: const MajorGridLines(width: 0),
-                            axisLine: const AxisLine(width: 0),
-                            labelPlacement: LabelPlacement.onTicks),
-                        primaryYAxis: NumericAxis(
-                            minimum: 0,
-                            maximum: 100,
-                            axisLine: const AxisLine(width: 0),
-                            majorTickLines: const MajorTickLines(size: 0),
-                        ),
-                        zoomPanBehavior: ZoomPanBehavior(
-                          enablePinching: true,
-                          zoomMode: ZoomMode.x,
-                          enablePanning: true,
-                          enableMouseWheelZooming: true,
-                        ),
-                        series: <CartesianSeries<ChartData, String>>[
-                          SplineRangeAreaSeries<ChartData, String>(
-                            color: const Color.fromRGBO(75, 135, 185, 0.5),
-                            borderColor: const Color.fromRGBO(75, 135, 185, 1),
-                            borderWidth: 3,
-                            dataSource: generateFocusTimeline(
-                                DateTime(2023, 05, 27, 7, 0)),
-                            xValueMapper: (ChartData data, _) => data.dataLabel as String,
-                            lowValueMapper: (ChartData data, _) => data.lowValue,
-                            highValueMapper: (ChartData data, _) => data.highValue,
-                            dataLabelMapper: (ChartData data, _) => data.dataLabel,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+                  path: "home", builder: (context, state) => const HomePage()),
               GoRoute(
                 path: "tasks",
-                builder: (context, state) =>
-                    const Scaffold(body: Center(child: Text("Tasks"))),
+                builder: (context, state) => const TasksPage(),
               ),
             ],
             builder: (context, state, child) {
@@ -84,8 +43,17 @@ final mainRouter = GoRouter(
                   onPressed: () => {context.go("/alfred")},
                   child: const Icon(Icons.mic_none_rounded),
                 ),
-                bottomNavigationBar: BottomAlfredBar(),
-                body: child,
+                bottomNavigationBar: MediaQuery.of(context).size.width < 640
+                    ? const BottomAlfredBar()
+                    : null,
+                body: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    if (MediaQuery.of(context).size.width >= 640)
+                      const AlfredRailNav(),
+                    Expanded(child: child),
+                  ],
+                ),
               );
             },
           ),
@@ -97,6 +65,54 @@ final mainRouter = GoRouter(
     ),
   ],
 );
+
+class AlfredRailNav extends StatefulWidget {
+  const AlfredRailNav({super.key});
+
+  @override
+  State<AlfredRailNav> createState() => _AlfredRailNavState();
+}
+
+class _AlfredRailNavState extends State<AlfredRailNav> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      minWidth: 85.0,
+      selectedIndex: _selectedIndex,
+      onDestinationSelected: (int e) {
+        setState(() {
+          _selectedIndex = e;
+        });
+        if (e == 0) {
+          context.go("/app/home");
+        } else if (e == 1) {
+          context.go("/app/tasks");
+        } else if (e == 2) {
+          context.go("/app/notes");
+        }
+      },
+      backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.05),
+      groupAlignment: 0,
+      labelType: NavigationRailLabelType.none,
+      destinations: const [
+        NavigationRailDestination(
+          icon: Icon(Icons.home_filled),
+          label: Placeholder(),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.task_alt_rounded),
+          label: Placeholder(),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.notes_rounded),
+          label: Placeholder(),
+        )
+      ],
+    );
+  }
+}
 
 class BottomAlfredBar extends StatefulWidget {
   const BottomAlfredBar({super.key});
@@ -125,7 +141,7 @@ class _BottomAlfredBarState extends State<BottomAlfredBar> {
       },
       destinations: const [
         NavigationDestination(
-          icon: Icon(Icons.home_rounded),
+          icon: Icon(Icons.home_filled),
           label: '',
         ),
         NavigationDestination(
