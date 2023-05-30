3e7154d2e9a47c5e8b71eb4885a217844952d5de
Piero Rolando <rolandopiero63@gmail.com>
2023-05-30 08:49:43 -0500

    Rename routes.dart file to router.dart and update import in main.dart to match the new file name

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:alfred/config/router/routes.dart';
+import 'package:alfred/config/router/router.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
