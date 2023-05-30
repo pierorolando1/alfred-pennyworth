b28a954e2e2224371b8eae098062fc33592b4341
Piero Rolando <rolandopiero63@gmail.com>
2023-05-30 08:16:41 -0500

    Import Alfred config router for better routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:alfred/config/router/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:alfred/config/router/routes.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
