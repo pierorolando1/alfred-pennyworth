954d5e7d822c61d3e92811f884e7b4fb90ea24d2
Piero Rolando <rolandopiero63@gmail.com>
2023-05-30 09:01:58 -0500

    Refactor navigation destinations to use a dynamic list of routes and icons in router.dart

--- lib/config/router/router.dart
+++ lib/config/router/router.dart
@@ -7,6 +7,13 @@ import 'package:alfred/features/tasks/presentation/pages/tasks_page.dart';
 import 'package:alfred/pages/home_page.dart';
 import 'package:alfred/utils/widgets/redirect.dart';
 
+// [ [route, icon, label] ]
+const List<List<dynamic>> navigations = [
+  ["/app/home", Icons.home_filled, "Home"],
+  ["/app/tasks", Icons.task_alt_rounded, "Tasks"],
+  ["/app/notes", Icons.notes_rounded, "Notes"],
+];
+
 final mainRouter = GoRouter(
   routes: [
     GoRoute(
@@ -24,41 +31,42 @@ final mainRouter = GoRouter(
       ),
     ),
     GoRoute(
-        path: "/app",
-        builder: (context, state) {
-          return const Redirect();
-        },
-        routes: [
-          ShellRoute(
-            routes: [
-              GoRoute(
-                  path: "home", builder: (context, state) => const HomePage()),
-              GoRoute(
-                path: "tasks",
-                builder: (context, state) => const TasksPage(),
+      path: "/app",
+      builder: (context, state) {
+        return const Redirect();
+      },
+      routes: [
+        ShellRoute(
+          routes: [
+            GoRoute(
+                path: "home", builder: (context, state) => const HomePage()),
+            GoRoute(
+              path: "tasks",
+              builder: (context, state) => const TasksPage(),
+            ),
+          ],
+          builder: (context, state, child) {
+            return Scaffold(
+              floatingActionButton: FloatingActionButton(
+                onPressed: () => {context.go("/alfred")},
+                child: const Icon(Icons.mic_none_rounded),
               ),
-            ],
-            builder: (context, state, child) {
-              return Scaffold(
-                floatingActionButton: FloatingActionButton(
-                  onPressed: () => {context.go("/alfred")},
-                  child: const Icon(Icons.mic_none_rounded),
-                ),
-                bottomNavigationBar: MediaQuery.of(context).size.width < 640
-                    ? const BottomAlfredBar()
-                    : null,
-                body: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    if (MediaQuery.of(context).size.width >= 640)
-                      const AlfredRailNav(),
-                    Expanded(child: child),
-                  ],
-                ),
-              );
-            },
-          ),
-        ]),
+              bottomNavigationBar: MediaQuery.of(context).size.width < 640
+                  ? const BottomAlfredBar()
+                  : null,
+              body: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  if (MediaQuery.of(context).size.width >= 640)
+                    const AlfredRailNav(),
+                  Expanded(child: child),
+                ],
+              ),
+            );
+          },
+        ),
+      ],
+    ),
     GoRoute(path: "/", builder: (context, state) => const LoadingPage()),
     GoRoute(
       path: "/login",
@@ -86,31 +94,22 @@ class _AlfredRailNavState extends State<AlfredRailNav> {
         setState(() {
           _selectedIndex = e;
         });
-        if (e == 0) {
-          context.go("/app/home");
-        } else if (e == 1) {
-          context.go("/app/tasks");
-        } else if (e == 2) {
-          context.go("/app/notes");
-        }
+
+        if (e == _selectedIndex) return;
+
+        context.go(navigations[e][0]);
       },
       backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.05),
       groupAlignment: 0,
       labelType: NavigationRailLabelType.none,
-      destinations: const [
-        NavigationRailDestination(
-          icon: Icon(Icons.home_filled),
-          label: Placeholder(),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.task_alt_rounded),
-          label: Placeholder(),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.notes_rounded),
-          label: Placeholder(),
-        )
-      ],
+      destinations: navigations
+          .map(
+            (n) => NavigationRailDestination(
+              icon: Icon(n[1]),
+              label: Text(n[2]),
+            ),
+          )
+          .toList(),
     );
   }
 }
@@ -133,27 +132,14 @@ class _BottomAlfredBarState extends State<BottomAlfredBar> {
         setState(() {
           selectedIndex = e;
         }),
-        if (e == 0)
-          {context.go("/app/home")}
-        else if (e == 1)
-          {context.go("/app/tasks")}
-        else if (e == 2)
-          {context.go("/app/notes")}
+        context.go(navigations[e][0]),
       },
-      destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.home_filled),
-          label: '',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.task_alt_rounded),
-          label: '',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.notes_rounded),
-          label: '',
-        )
-      ],
+      destinations: navigations
+          .map((n) => NavigationDestination(
+                icon: Icon(n[1]),
+                label: n[2],
+              ))
+          .toList(),
     );
   }
 }
